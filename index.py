f628f80970e5fab40f0cab7f27d14dd25f45f791
Aleksander Ujek <aleksanderujek@gmail.com>
2020-06-14 16:38:53 +0200

    feat(individual): created fitness function and weight function

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
 from models.individual import Individual
 from models.population import Population
+import numpy as np
 
 newIndividual = Individual(10)
 print(newIndividual)
 print(newIndividual.knapsack)
 
 
+cost_list = np.full(10, [1,2,3,4,5,6,7,8,9,10])
+weight_list = np.full(10, [1,2,1,2,1,2,1,2,1,2])
+
 population = Population(15,10)
 print(population)
 for element in population.individuals:
-    print(element.knapsack)
+    print("TABLE: {0}  NUM_OF_ELEMENTS: {1} FITNESS: {2} WEIGHT: {3}").format(element.knapsack, element.num_of_elements(), element.calculate_fitness(cost_list), element.calculate_weight(weight_list))
--- models/individual.py
+++ models/individual.py
@@ -2,8 +2,19 @@ import numpy as np
 
 class Individual:
     knapsack = np.array([])
-
+    fitness = 0
+    weight = 0
 
     def __init__(self, n):
         self.knapsack = np.random.choice([False, True], size=n)
         
+    def num_of_elements(self):
+        return np.sum(self.knapsack)
+
+    def calculate_fitness(self, cost_list):
+        self.fitness = np.dot(self.knapsack, cost_list)
+        return self.fitness
+
+    def calculate_weight(self, weight_list):
+        self.weight = np.dot(self.knapsack, weight_list)
+        return self.weight
