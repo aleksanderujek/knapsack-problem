596d7839c03860fb72d4c8ad801ce68e1a887c64
Aleksander Ujek <aleksanderujek@gmail.com>
2020-06-22 20:56:37 +0200

    feat(ga): created new data and added global best find instead of best from last generation

--- index.py
+++ index.py
@@ -10,33 +10,35 @@ import matplotlib.pyplot as plt
 num_of_elements = 10
 newIndividual = Individual(num_of_elements)
 
-
-cost_list = np.full(num_of_elements, [1,2,3,4,5,6,7,8,9,10])
-weight_list = np.full(num_of_elements, [1,2,1,2,1,2,1,2,1,2])
-max_weight = 10
-population_size = 100
-generations = 1000
+cost_list = np.full(num_of_elements, [10,15,20,6,3,23,7,11,1,5])
+weight_list = np.full(num_of_elements, [10,14,15,3,1,13,3,12,1,10])
+max_weight = 40
+population_size = 50
+generations = 10000
 
 population = Population(population_size,num_of_elements)
 
 def cycle(population, cost_list, weight_list, max_weight, crossover_ratio, mutation_ratio, generationIndex = 0, breakPoint = 50):
     fitness = population.calculate_fitness(cost_list, weight_list, max_weight)
+    best = population.get_best()
     selections = selection(population, generationIndex, breakPoint)
     crossovers = crossover(selections, crossover_ratio)
     mutations = mutation(crossovers, mutation_ratio)
-    return mutations, fitness
+    return mutations, fitness, best
 
 
 fitness_history = np.empty((generations,population_size), dtype=int)
 
+best = Individual(None)
+
 for i in tqdm(range(generations)):
-    population, fitness = cycle(population, cost_list, weight_list, max_weight, 0.9, 0.7, i,(generations*3)/4)
+    population, fitness, currentBest = cycle(population, cost_list, weight_list, max_weight, 0.9, 0.4, i,(generations*3)/4)
+    if (currentBest.fitness > best.fitness):
+        best = currentBest
     fitness_history[i] = fitness
 
 
 print("================================")
-population.calculate_fitness(cost_list, weight_list, max_weight)
-best = population.get_best()
 print("Best: {0}, NUM_OF_ELEMENTS: {1} FITNESS: {2} WEIGHT: {3}".format(best.knapsack, best.num_of_elements(), best.fitness, best.weight))
 print("================================")
 
