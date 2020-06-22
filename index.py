7f9727cdb33fc67b8db1601e011fc574a129dc6a
Aleksander Ujek <aleksanderujek@gmail.com>
2020-06-22 20:30:17 +0200

    improvement(ga): added num of elements variable and cleared project from commented code

--- index.py
+++ index.py
@@ -7,38 +7,17 @@ from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 
-newIndividual = Individual(10)
-
-
-cost_list = np.full(10, [1,2,3,4,5,6,7,8,9,10])
-weight_list = np.full(10, [1,2,1,2,1,2,1,2,1,2])
-max_weight = 10
 num_of_elements = 10
+newIndividual = Individual(num_of_elements)
+
+
+cost_list = np.full(num_of_elements, [1,2,3,4,5,6,7,8,9,10])
+weight_list = np.full(num_of_elements, [1,2,1,2,1,2,1,2,1,2])
+max_weight = 10
 population_size = 100
 generations = 1000
 
 population = Population(population_size,num_of_elements)
-# population.calculate_fitness(cost_list, weight_list, max_weight)
-# for i, element in enumerate(population.individuals):
-#     print("{0} TABLE: {1}  NUM_OF_ELEMENTS: {2} FITNESS: {3} WEIGHT: {4}").format(i, element.knapsack, element.num_of_elements(), element.fitness, element.weight)
-
-# print("================================")
-# best = population.get_best()
-# print("Best: {0}, NUM_OF_ELEMENTS: {1} FITNESS: {2} WEIGHT: {3}").format(best.knapsack, best.num_of_elements(), best.fitness, best.weight)
-# print("================================")
-
-
-# selections = selection(population, 0)
-# for i, element in enumerate(selections.individuals):
-#     print("{0} TABLE: {1}  NUM_OF_ELEMENTS: {2} FITNESS: {3} WEIGHT: {4}").format(i, element.knapsack, element.num_of_elements(), element.fitness, element.weight)
-
-# crossovers = crossover(selections)
-
-# for i, element in enumerate(crossovers.individuals):
-#     print("{0} TABLE: {1}  NUM_OF_ELEMENTS: {2} FITNESS: {3} WEIGHT: {4}").format(i, element.knapsack, element.num_of_elements(), element.calculate_fitness(cost_list,weight_list, max_weight)
-# , element.weight)
-
-# mutations = mutation(crossovers)
 
 def cycle(population, cost_list, weight_list, max_weight, crossover_ratio, mutation_ratio, generationIndex = 0, breakPoint = 50):
     fitness = population.calculate_fitness(cost_list, weight_list, max_weight)
